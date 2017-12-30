32e4d78391d66c659c65247c073786ce4b95dd16
littlewhiteball <narisong@gmail.com>
2017-12-30 01:22:36 -0800

    Beautified competition summary

--- src/components/competitions/CompetitionSummary.jsx
+++ src/components/competitions/CompetitionSummary.jsx
@@ -4,12 +4,26 @@ import PropTypes from 'prop-types';
 export default class CompetitionSummary extends PureComponent {
     render() {
         // TODO: Create util class to handle datetime conversion
-        const start = this.props.competition.start.toLocaleString();
-        const closing = this.props.competition.closing.toLocaleString();
-        const summary = `${this.props.competition.title}\nstart:${start}\nend:${closing}`;
+        const start = new Date(this.props.competition.start).toLocaleString();
+        const closing = new Date(this.props.competition.closing).toLocaleString();
+        // TODO: Get count of each option
         return (
-            <div className="card card-block">
-                {summary}
+            <div className="jumbotron mt-2 col-9">
+                <small>start: {start}</small>
+                <br />
+                <small>closing: {closing}</small>
+                <ul className="list-group">
+                    {
+                        this.props.competition.options.map(option => (
+                            <li className="list-group-item d-flex justify-content-between align-items-center">
+                                {option}
+                                <span className="badge badge-primary badge-pill">
+                                    10
+                                </span>
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         );
     }
