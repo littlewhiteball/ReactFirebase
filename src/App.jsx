ecd7dd993db1926e1649bf06257ccb4aeba704ca
littlewhiteball <narisong@gmail.com>
2018-01-11 23:20:05 -0800

    Added route for creating new competition

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './components/main/Home';
 import Auth from './components/pages/Auth';
+import NewCompetition from './components/pages/NewCompetition';
 
 export default () => (
     <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/auth" exact component={Auth} />
+        <Route path="/competition" exact component={NewCompetition} />
     </Switch>
 );
