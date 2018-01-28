1ed5d215753055b9f7eedb683cd833e7bb06f8d3
littlewhiteball <narisong@gmail.com>
2018-01-28 15:55:30 -0800

    Added user reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,13 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import competitions from './competitionsReducer';
+import user from './userReducer';
+// TODO: remove auth
 import auth from './authReducer';
 
 export default combineReducers({
     routing: routerReducer,
     competitions,
+    user,
     auth,
 });
