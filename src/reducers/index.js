2cfa7a44d331ca428cf846e0066d51f31ce79e04
littlewhiteball <narisong@gmail.com>
2018-01-28 21:01:45 -0800

    Removed auth action and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,9 @@ import { routerReducer } from 'react-router-redux';
 
 import competitions from './competitionsReducer';
 import user from './userReducer';
-// TODO: remove auth
-import auth from './authReducer';
 
 export default combineReducers({
     routing: routerReducer,
     competitions,
     user,
-    auth,
 });
