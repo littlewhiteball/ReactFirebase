e620578b50ef1698608047c4ce4201b56a5559e9
littlewhiteball <narisong@gmail.com>
2018-02-03 21:22:01 -0800

    Added description and fulfillment in competition db model test

--- src/database/models/__tests__/competitionsDbModel.test.js
+++ src/database/models/__tests__/competitionsDbModel.test.js
@@ -5,8 +5,10 @@ describe('competitions db model', () => {
         const expected = {
             id: 'competitiondbmodelid',
             title: 'competition db model',
+            description: 'competition db model description',
             start: new Date('2018-01-02T10:00:00z'),
             closing: new Date('2018-01-04T10:00:00z'),
+            fulfillment: new Date('2018-01-05T10:00:00z'),
             options: [
                 'db model option 1',
                 'db model option 2',
@@ -16,8 +18,10 @@ describe('competitions db model', () => {
         expect(competitionsDbModel(
             'competitiondbmodelid',
             'competition db model',
+            'competition db model description',
             new Date('2018-01-02T10:00:00z'),
             new Date('2018-01-04T10:00:00z'),
+            new Date('2018-01-05T10:00:00z'),
             ['db model option 1', 'db model option 2'],
         )).toEqual(expected);
     });
