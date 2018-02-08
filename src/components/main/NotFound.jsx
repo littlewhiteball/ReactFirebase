f683101b8feac845d6a51a49837b0c3442d8fa6e
littlewhiteball <narisong@gmail.com>
2018-02-08 00:26:15 -0800

    Added redirect after 3 seconds in not found page

--- src/components/main/NotFound.jsx
+++ src/components/main/NotFound.jsx
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
-const NOT_FOUND_TEXT = 'Not found, redirecting back to home page';
+const NOT_FOUND_TEXT = 'Page not found, redirecting back to home page in 3 seconds';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class NotFound extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = ({
+            show: false,
+        });
+    }
+
+    componentWillMount() {
+        setTimeout(() => (
+            this.setState({
+                show: true,
+            })
+        ), 3000);
+    }
+
     render() {
         return (
-            <div>
-                <h1>{NOT_FOUND_TEXT}</h1>
-                <Redirect to="/" />
+            <div className="container">
+                <h4 className="text-primary my-4">{NOT_FOUND_TEXT}</h4>
+                {
+                    this.state.show ?
+                        <Redirect to="/" /> :
+                        null
+                }
             </div>
         );
     }
--- src/components/main/__tests__/NotFound.test.jsx
+++ src/components/main/__tests__/NotFound.test.jsx
@@ -7,11 +7,32 @@ import NotFound from './../NotFound';
 
 configure({ adapter: new Adapter() });
 
+jest.useFakeTimers();
+
 const wrapper = shallow(<NotFound />);
 
 describe('not found component', () => {
     it('should render self', () => {
-        expect(wrapper.find('h1').text()).toBe('Not found, redirecting back to home page');
-        expect(wrapper.find(Redirect).exists()).toBe(true);
+        expect(wrapper.find('div').hasClass('container')).toBe(true);
+        expect(wrapper.find('h4').hasClass('text-primary my-4')).toBe(true);
+        expect(wrapper.find('h4').text()).toBe('Page not found, redirecting back to home page in 3 seconds');
+    });
+
+    it('initial state', () => {
+        expect(wrapper.state('show')).toBe(false);
+    });
+
+    it('should not render redirect initially', () => {
+        expect(wrapper.find(Redirect).exists()).toBe(false);
+    });
+
+    it('should render redirect after 3 seconds', () => {
+        expect(setTimeout).toHaveBeenCalledTimes(1);
+        expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 3000);
+
+        jest.runAllTimers();
+
+        expect(wrapper.state('show')).toBe(true);
+        expect(wrapper.update().find(Redirect).exists()).toBe(true);
     });
 });
