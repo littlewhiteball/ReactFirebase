b166957cca1708098e81781cec8aa6f2377419b5
littlewhiteball <narisong@gmail.com>
2018-03-17 13:04:28 -0700

    Added UI Component validation by bootstrap, for AddCompetitionForm component

--- src/components/competitions/AddCompetitionForm.jsx
+++ src/components/competitions/AddCompetitionForm.jsx
@@ -13,7 +13,9 @@ const COMPETITION_INFORMATION = 'Competition Information';
 const TITLE = 'Title';
 const TITLE_PLACEHOLDER = 'Competition title';
 const TITLE_ID = 'Title';
-const DESCRIPTION = 'Description';
+const TITLE_INVALID_FEEDBACK = 'Please provide a title for your competition';
+const TITLE_MAX_LENGTH = 256;
+const DESCRIPTION = 'Description (Optional)';
 const DESCRIPTION_PLACEHOLDER = 'Competition description';
 const DESCRIPTION_ID = 'Description';
 const PUBLIC = getText(CompetitionVisibilityEnum.PUBLIC);
@@ -24,12 +26,17 @@ const VISIBILITY = 'Visibility';
 const VISIBILITY_ID = 'Visibility';
 const ENTRIES_CLOSE = 'Entries Close';
 const ENTRIES_CLOSE_ID = 'EntriesClose';
+const ENTRIES_CLOSE_INVALID_FEEDBACK = 'Please provide a valid datetime in the format of MM/dd/yyyy hh:mm AM/PM';
 const FULFILLMENT_DATE = 'Fulfillment Date';
 const FULFILLMENT_DATE_ID = 'FulfillmentDate';
+const FULFILLMENT_DATE_INVALID_FEEDBACK = 'Please provide a valid datetime in the format of MM/dd/yyyy hh:mm AM/PM';
 const OPTIONS = 'Options';
 // TODO: Temporarily using comma separated text as list of options
 const OPTIONS_PLACEHOLDER = 'Option1,Option2';
 const OPTIONS_ID = 'Options';
+const OPTIONS_INVALID_FEEDBACK = 'Please provide a list of comma separated options without whitespaces';
+// TODO: temp: no whitespaces allowed for options
+const OPTIONS_REGEX = '^\\S*$';
 const SAVE = 'Save';
 const CANCEL = 'Cancel';
 
@@ -37,6 +44,7 @@ export class AddCompetitionFormComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            formClass: 'needs-validation',
             saving: false,
             title: '',
             description: '',
@@ -46,6 +54,18 @@ export class AddCompetitionFormComponent extends Component {
             fulfillment: moment().add(2, 'days').toDate(),
             options: [],
         };
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (e.target.checkValidity() === true) {
+            this.saveCompetition();
+        } else {
+            this.setState({
+                formClass: 'was-validated',
+            });
+        }
     }
 
     updateTitle = (e) => {
@@ -140,11 +160,14 @@ export class AddCompetitionFormComponent extends Component {
                                         <h3>{COMPETITION_INFORMATION}</h3>
                                     </div>
                                     <div className="card-block">
-                                        <form className="form" autoComplete="off">
+                                        <form className={this.state.formClass} onSubmit={this.handleFormSubmit} noValidate autoComplete="off">
                                             <div className="form-group row">
                                                 <label className="col-lg-3 col-form-label form-control-label" htmlFor={TITLE_ID}>{TITLE}</label>
                                                 <div className="col-lg-9">
-                                                    <input className="form-control" type="text" id={TITLE_ID} placeholder={TITLE_PLACEHOLDER} onChange={this.updateTitle} />
+                                                    <input className="form-control" type="text" id={TITLE_ID} placeholder={TITLE_PLACEHOLDER} onChange={this.updateTitle} required maxLength={TITLE_MAX_LENGTH} />
+                                                    <div className="invalid-feedback">
+                                                        {TITLE_INVALID_FEEDBACK}
+                                                    </div>
                                                 </div>
                                             </div>
                                             <div className="form-group row">
@@ -166,23 +189,32 @@ export class AddCompetitionFormComponent extends Component {
                                                 <label className="col-lg-3 col-form-label form-control-label" htmlFor={ENTRIES_CLOSE_ID}>{ENTRIES_CLOSE}</label>
                                                 <div className="col-lg-9" id={ENTRIES_CLOSE_ID}>
                                                     <DateTimePicker defaultDateTimeValue={this.state.entriesClose} onChange={this.updateEntriesClose} />
+                                                    <div className="invalid-feedback">
+                                                        {ENTRIES_CLOSE_INVALID_FEEDBACK}
+                                                    </div>
                                                 </div>
                                             </div>
                                             <div className="form-group row">
                                                 <label className="col-lg-3 col-form-label form-control-label" htmlFor={FULFILLMENT_DATE_ID}>{FULFILLMENT_DATE}</label>
                                                 <div className="col-lg-9" id={FULFILLMENT_DATE_ID}>
                                                     <DateTimePicker defaultDateTimeValue={this.state.fulfillment} onChange={this.updateFulfillmentDate} />
+                                                    <div className="invalid-feedback">
+                                                        {FULFILLMENT_DATE_INVALID_FEEDBACK}
+                                                    </div>
                                                 </div>
                                             </div>
                                             <div className="form-group row">
                                                 <label className="col-lg-3 col-form-label form-control-label" htmlFor={OPTIONS_ID}>{OPTIONS}</label>
                                                 <div className="col-lg-9" id={OPTIONS_ID}>
-                                                    <input className="form-control" type="text" id={OPTIONS_ID} placeholder={OPTIONS_PLACEHOLDER} onChange={this.updateOptions} />
+                                                    <input className="form-control" type="text" id={OPTIONS_ID} placeholder={OPTIONS_PLACEHOLDER} onChange={this.updateOptions} required pattern={OPTIONS_REGEX} />
+                                                    <div className="invalid-feedback">
+                                                        {OPTIONS_INVALID_FEEDBACK}
+                                                    </div>
                                                 </div>
                                             </div>
                                             <div className="form-group row">
                                                 <div className="col-lg-4 offset-lg-8 row">
-                                                    <button className="col-lg-6 form-control btn btn-primary" type="button" onClick={this.saveCompetition}>
+                                                    <button className="col-lg-6 form-control btn btn-primary" type="submit" >
                                                         {
                                                             this.state.saving ?
                                                                 <i className="fa fa-spinner fa-spin" /> :
--- src/components/competitions/__tests__/AddCompetitionForm.test.jsx
+++ src/components/competitions/__tests__/AddCompetitionForm.test.jsx
@@ -45,8 +45,9 @@ describe('add competition form component', () => {
         expect(wrapper.find('div').at(6).hasClass('card-header mb-2')).toBe(true);
         expect(wrapper.find('h3').text()).toBe('Competition Information');
         expect(wrapper.find('div').at(7).hasClass('card-block')).toBe(true);
-        expect(wrapper.find('form').hasClass('form')).toBe(true);
+        expect(wrapper.find('form').hasClass('needs-validation')).toBe(true);
         expect(wrapper.find('form').prop('autoComplete')).toBe('off');
+        expect(wrapper.find('form').prop('noValidate')).toBe(true);
         expect(wrapper.find('div').at(8).hasClass('form-group row')).toBe(true);
         expect(wrapper.find('label').at(0).hasClass('col-lg-3 col-form-label form-control-label')).toBe(true);
         expect(wrapper.find('label').at(0).prop('htmlFor')).toBe('Title');
@@ -56,20 +57,24 @@ describe('add competition form component', () => {
         expect(wrapper.find('input').at(0).prop('type')).toBe('text');
         expect(wrapper.find('input').at(0).prop('id')).toBe('Title');
         expect(wrapper.find('input').at(0).prop('placeholder')).toBe('Competition title');
-        expect(wrapper.find('div').at(10).hasClass('form-group row')).toBe(true);
+        expect(wrapper.find('input').at(0).prop('required')).toBe(true);
+        expect(wrapper.find('input').at(0).prop('maxLength')).toBe(256);
+        expect(wrapper.find('div').at(10).hasClass('invalid-feedback')).toBe(true);
+        expect(wrapper.find('div').at(10).text()).toBe('Please provide a title for your competition');
+        expect(wrapper.find('div').at(11).hasClass('form-group row')).toBe(true);
         expect(wrapper.find('label').at(1).hasClass('col-lg-3 col-form-label form-control-label')).toBe(true);
         expect(wrapper.find('label').at(1).prop('htmlFor')).toBe('Description');
-        expect(wrapper.find('label').at(1).text()).toBe('Description');
-        expect(wrapper.find('div').at(11).hasClass('col-lg-9')).toBe(true);
+        expect(wrapper.find('label').at(1).text()).toBe('Description (Optional)');
+        expect(wrapper.find('div').at(12).hasClass('col-lg-9')).toBe(true);
         expect(wrapper.find('input').at(1).hasClass('form-control')).toBe(true);
         expect(wrapper.find('input').at(1).prop('type')).toBe('text');
         expect(wrapper.find('input').at(1).prop('id')).toBe('Description');
         expect(wrapper.find('input').at(1).prop('placeholder')).toBe('Competition description');
-        expect(wrapper.find('div').at(12).hasClass('form-group row')).toBe(true);
+        expect(wrapper.find('div').at(13).hasClass('form-group row')).toBe(true);
         expect(wrapper.find('label').at(2).hasClass('col-lg-3 col-form-label form-control-label')).toBe(true);
         expect(wrapper.find('label').at(2).prop('htmlFor')).toBe('Visibility');
         expect(wrapper.find('label').at(2).text()).toBe('Visibility');
-        expect(wrapper.find('div').at(13).hasClass('col-lg-9')).toBe(true);
+        expect(wrapper.find('div').at(14).hasClass('col-lg-9')).toBe(true);
         expect(wrapper.find('input').at(2).hasClass('mr-1')).toBe(true);
         expect(wrapper.find('input').at(2).prop('name')).toBe('Visibility');
         expect(wrapper.find('input').at(2).prop('type')).toBe('radio');
@@ -85,31 +90,39 @@ describe('add competition form component', () => {
         expect(wrapper.find('input').at(3).prop('checked')).toBe(undefined);
         expect(wrapper.find('label').at(4).prop('htmlFor')).toBe('Private');
         expect(wrapper.find('label').at(4).text()).toBe('Private');
-        expect(wrapper.find('div').at(14).hasClass('form-group row')).toBe(true);
+        expect(wrapper.find('div').at(15).hasClass('form-group row')).toBe(true);
         expect(wrapper.find('label').at(5).hasClass('col-lg-3 col-form-label form-control-label')).toBe(true);
         expect(wrapper.find('label').at(5).prop('htmlFor')).toBe('EntriesClose');
         expect(wrapper.find('label').at(5).text()).toBe('Entries Close');
-        expect(wrapper.find('div').at(15).hasClass('col-lg-9')).toBe(true);
+        expect(wrapper.find('div').at(16).hasClass('col-lg-9')).toBe(true);
         expect(wrapper.find('datetimepicker').exists()).toBe(true);
-        expect(wrapper.find('div').at(16).hasClass('form-group row')).toBe(true);
+        expect(wrapper.find('div').at(17).hasClass('invalid-feedback')).toBe(true);
+        expect(wrapper.find('div').at(17).text()).toBe('Please provide a valid datetime in the format of MM/dd/yyyy hh:mm AM/PM');
+        expect(wrapper.find('div').at(18).hasClass('form-group row')).toBe(true);
         expect(wrapper.find('label').at(6).hasClass('col-lg-3 col-form-label form-control-label')).toBe(true);
         expect(wrapper.find('label').at(6).prop('htmlFor')).toBe('FulfillmentDate');
         expect(wrapper.find('label').at(6).text()).toBe('Fulfillment Date');
-        expect(wrapper.find('div').at(17).hasClass('col-lg-9')).toBe(true);
+        expect(wrapper.find('div').at(19).hasClass('col-lg-9')).toBe(true);
         expect(wrapper.find('datetimepicker').exists()).toBe(true);
-        expect(wrapper.find('div').at(18).hasClass('form-group row')).toBe(true);
+        expect(wrapper.find('div').at(20).hasClass('invalid-feedback')).toBe(true);
+        expect(wrapper.find('div').at(20).text()).toBe('Please provide a valid datetime in the format of MM/dd/yyyy hh:mm AM/PM');
+        expect(wrapper.find('div').at(21).hasClass('form-group row')).toBe(true);
         expect(wrapper.find('label').at(7).hasClass('col-lg-3 col-form-label form-control-label')).toBe(true);
         expect(wrapper.find('label').at(7).prop('htmlFor')).toBe('Options');
         expect(wrapper.find('label').at(7).text()).toBe('Options');
-        expect(wrapper.find('div').at(19).hasClass('col-lg-9')).toBe(true);
+        expect(wrapper.find('div').at(22).hasClass('col-lg-9')).toBe(true);
         expect(wrapper.find('input').at(4).hasClass('form-control')).toBe(true);
         expect(wrapper.find('input').at(4).prop('type')).toBe('text');
         expect(wrapper.find('input').at(4).prop('id')).toBe('Options');
         expect(wrapper.find('input').at(4).prop('placeholder')).toBe('Option1,Option2');
-        expect(wrapper.find('div').at(20).hasClass('form-group row')).toBe(true);
-        expect(wrapper.find('div').at(21).hasClass('col-lg-4 offset-lg-8 row')).toBe(true);
+        expect(wrapper.find('input').at(4).prop('required')).toBe(true);
+        expect(wrapper.find('input').at(4).prop('pattern')).toBe('^\\S*$');
+        expect(wrapper.find('div').at(23).hasClass('invalid-feedback')).toBe(true);
+        expect(wrapper.find('div').at(23).text()).toBe('Please provide a list of comma separated options without whitespaces');
+        expect(wrapper.find('div').at(24).hasClass('form-group row')).toBe(true);
+        expect(wrapper.find('div').at(25).hasClass('col-lg-4 offset-lg-8 row')).toBe(true);
         expect(wrapper.find('button').at(0).hasClass('col-lg-6 form-control btn btn-primary')).toBe(true);
-        expect(wrapper.find('button').at(0).prop('type')).toBe('button');
+        expect(wrapper.find('button').at(0).prop('type')).toBe('submit');
         expect(wrapper.find('button').at(0).text()).toBe('Save');
         expect(wrapper.find('button').at(1).hasClass('col-lg-6 form-control btn btn-default')).toBe(true);
         expect(wrapper.find('button').at(1).prop('type')).toBe('button');
@@ -127,6 +140,7 @@ describe('add competition form component', () => {
 
     it('initial state', () => {
         const expectedState = {
+            formClass: 'needs-validation',
             saving: false,
             title: '',
             description: '',
@@ -189,16 +203,30 @@ describe('add competition form component', () => {
         expect(wrapper.state('options')).toEqual(['option1', 'option2']);
     });
 
-    it('should set saving state when save is clicked', () => {
+    it('should set saving state when save is clicked and form is validated', () => {
         const { wrapper } = setup();
-        wrapper.setState({
-            name: 'competition 0',
-            description: 'competition 0 description',
-            options: ['option1', 'option2'],
+        wrapper.find('button').at(0).simulate('submit', {
+            target: {
+                name: 'form',
+                checkValidity: () => true,
+            },
         });
-        wrapper.find('button').at(0).simulate('click', { preventDefault() { } });
 
         expect(wrapper.state('saving')).toBe(true);
+    });
+
+    it('should set formClass state to was-validated and not save change when save is clicked but form is not validated', () => {
+        const { props, wrapper } = setup();
+        wrapper.find('button').at(0).simulate('submit', {
+            target: {
+                name: 'form',
+                checkValidity: () => false,
+            },
+        });
+
+        expect(wrapper.state('formClass')).toBe('was-validated');
+        expect(wrapper.state('saving')).toBe(false);
+        expect(props.saveChange.mock.calls.length).toBe(0);
     });
 
     it('should call props save change when save is clicked', () => {
@@ -231,7 +259,12 @@ describe('add competition form component', () => {
                 value: 'option1,option2',
             },
         });
-        wrapper.find('button').at(0).simulate('click', { preventDefault() { } });
+        wrapper.find('button').at(0).simulate('submit', {
+            target: {
+                name: 'form',
+                checkValidity: () => true,
+            },
+        });
 
         expect(props.saveChange.mock.calls.length).toBe(1);
         expect(props.saveChange.mock.calls[0][0]).toEqual(expectedArguments);
@@ -239,7 +272,12 @@ describe('add competition form component', () => {
 
     it('should call props nav to home after save succeeded', () => {
         const { props, wrapper } = setup();
-        wrapper.find('button').at(0).simulate('click', { preventDefault() { } });
+        wrapper.find('button').at(0).simulate('submit', {
+            target: {
+                name: 'form',
+                checkValidity: () => true,
+            },
+        });
 
         expect(props.saveChange.mock.calls.length).toBe(1);
         expect(props.navToPath.mock.calls.length).toBe(1);
