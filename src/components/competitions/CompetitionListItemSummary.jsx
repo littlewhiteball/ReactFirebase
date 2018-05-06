20bb9e0eea4ee6354e7622387ecd5ebd074c0f8d
littlewhiteball <narisong@gmail.com>
2018-05-06 00:16:30 -0700

    Show vote count and sum of bet values in competition summary component

--- src/components/competitions/CompetitionListItemSummary.jsx
+++ src/components/competitions/CompetitionListItemSummary.jsx
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import competitionEntriesDbAdapter from './../../database/competitionEntriesDbAdapter';
+
+import mapReduce from './../../utilities/mapReduce';
+
 // eslint-disable-next-line react/prefer-stateless-function
 export default class CompetitionListItemSummary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            competitionEntriesLoaded: false,
+        };
+    }
+
+    componentWillMount() {
+        this.optionCountAndValues = {};
+
+        competitionEntriesDbAdapter
+            .getCompetitionEntriesOnceFromDb(this.props.competition.id)
+            .then((snapshot) => {
+                const entries = snapshot.val();
+                if (entries) {
+                    this.optionCountAndValues = mapReduce.groupByAndSum(entries, 'option', 'value');
+                }
+
+                this.setState({
+                    competitionEntriesLoaded: true,
+                });
+            });
+    }
+
+    getCountAndValuePill = option => (
+        <div>
+            <span className="badge badge-primary badge-pill">
+                {option in this.optionCountAndValues ? this.optionCountAndValues[option].count : 0}
+            </span>
+            <span className="badge badge-primary badge-pill ml-2">
+                ${option in this.optionCountAndValues ? this.optionCountAndValues[option].sum : 0}
+            </span>
+        </div>
+    );
+
     render() {
         // TODO: Create util class to handle datetime conversion
         const start = new Date(this.props.competition.start).toLocaleString();
         const closing = new Date(this.props.competition.closing).toLocaleString();
-        // TODO: Get count of each option
+
         return (
             <div className="jumbotron py-2 mt-2 col-9">
                 <small>start: {start}</small>
@@ -20,9 +59,11 @@ export default class CompetitionListItemSummary extends Component {
                             return (
                                 <li className="list-group-item d-flex justify-content-between align-items-center" key={optionId}>
                                     <span>{option}</span>
-                                    <span className="badge badge-primary badge-pill">
-                                        {i}
-                                    </span>
+                                    {
+                                        this.state.competitionEntriesLoaded
+                                            ? this.getCountAndValuePill(option)
+                                            : null
+                                    }
                                 </li>
                             );
                         })
--- src/components/competitions/__tests__/CompetitionListItemSummary.test.jsx
+++ src/components/competitions/__tests__/CompetitionListItemSummary.test.jsx
@@ -6,6 +6,8 @@ import * as testConstants from './../../../__tests_constants__';
 
 import CompetitionListItemSummary from './../CompetitionListItemSummary';
 
+jest.mock('./../../../firebase');
+
 configure({ adapter: new Adapter() });
 
 const setup = () => {
@@ -22,6 +24,7 @@ const setup = () => {
 describe('competition summary component', () => {
     it('should render self', () => {
         const { wrapper } = setup();
+
         expect(wrapper.find('div').hasClass('jumbotron py-2 mt-2 col-9')).toBe(true);
         expect(wrapper.find('small').at(0).text()).toBe('start: 2017-11-2 02:00:00');
         expect(wrapper.find('br').exists()).toBe(true);
@@ -30,12 +33,37 @@ describe('competition summary component', () => {
         expect(wrapper.find('li').at(0).hasClass('list-group-item d-flex justify-content-between align-items-center')).toBe(true);
         expect(wrapper.find('li').at(0).key()).toBe('competition0idcompetition0idoption0');
         expect(wrapper.find('span').at(0).text()).toBe('Houston Astros');
-        expect(wrapper.find('span').at(1).hasClass('badge badge-primary badge-pill')).toBe(true);
-        expect(wrapper.find('span').at(1).text()).toBe('0');
         expect(wrapper.find('li').at(1).hasClass('list-group-item d-flex justify-content-between align-items-center')).toBe(true);
         expect(wrapper.find('li').at(1).key()).toBe('competition0idcompetition0idoption1');
-        expect(wrapper.find('span').at(2).text()).toBe('LA Dodgers');
-        expect(wrapper.find('span').at(3).hasClass('badge badge-primary badge-pill')).toBe(true);
-        expect(wrapper.find('span').at(3).text()).toBe('1');
+        expect(wrapper.find('span').at(1).text()).toBe('LA Dodgers');
+    });
+
+    it('initial state', () => {
+        const { wrapper } = setup();
+
+        expect(wrapper.state()).toEqual({
+            competitionEntriesLoaded: false,
+        });
+    });
+
+    it('should set competitionEntriesLoaded state and render badge pills for entry count and sum after component is mounted', () => {
+        const { wrapper } = setup();
+
+        setImmediate(() => {
+            // TODO: the following line was not needed in other tests (e.g. EditProfile)
+            wrapper.update();
+
+            expect(wrapper.state('competitionEntriesLoaded')).toBe(true);
+            expect(wrapper.find('span').at(0).text()).toBe('Houston Astros');
+            expect(wrapper.find('span').at(1).hasClass('badge badge-primary badge-pill')).toBe(true);
+            expect(wrapper.find('span').at(1).text()).toBe('1');
+            expect(wrapper.find('span').at(2).hasClass('badge badge-primary badge-pill ml-2')).toBe(true);
+            expect(wrapper.find('span').at(2).text()).toBe('$1');
+            expect(wrapper.find('span').at(3).text()).toBe('LA Dodgers');
+            expect(wrapper.find('span').at(4).hasClass('badge badge-primary badge-pill')).toBe(true);
+            expect(wrapper.find('span').at(4).text()).toBe('0');
+            expect(wrapper.find('span').at(5).hasClass('badge badge-primary badge-pill ml-2')).toBe(true);
+            expect(wrapper.find('span').at(5).text()).toBe('$0');
+        });
     });
 });
